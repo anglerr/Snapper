25f1a958e3cca6f11398660c953ebb941fdfcefc
Aditya Mehta <aadityamehta15@gmail.com>
2020-07-15 20:46:01 +0530

    Changes: 1. Updated HOST to https://anglerr.us

--- urlshortner/constants.py
+++ urlshortner/constants.py
@@ -2,5 +2,5 @@ import os
 
 
 SECRETS_LENGTH = 6
-HOST = os.environ.get("HOST", "http://localhost:8111/")  # TODO change this according to your deployment DNS name
+HOST = os.environ.get("HOST", "https://anglerr.us/")  # TODO change this according to your deployment DNS name
 
